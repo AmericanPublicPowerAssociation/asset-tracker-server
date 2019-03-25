c66397c5ec9099a0ec44cbf538fe6a9c113254af
Roy Hyunjin Han <rhh@crosscompute.com>
2019-03-25 08:50:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+from os.path import abspath, dirname, join
 from setuptools import find_packages, setup
 
 
@@ -8,52 +8,50 @@ initialize_asset_tracker_db=asset_tracker.scripts.initialize_db:main
 [paste.app_factory]
 main = asset_tracker:main
 """
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
-
-requires = [
+APP_CLASSIFIERS = [
+    'Programming Language :: Python',
+    'Framework :: Pyramid',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+]
+APP_REQUIREMENTS = [
+    # Web
     'plaster_pastedeploy',
     'pyramid',
     'pyramid_jinja2',
     'pyramid_debugtoolbar',
     'waitress',
+    # Database
     'alembic',
     'pyramid_retry',
     'pyramid_tm',
-    'SQLAlchemy',
+    'sqlalchemy',
     'transaction',
     'zope.sqlalchemy',
 ]
-
-tests_require = [
-    'WebTest >= 1.3.1',  # py3 compat
+TEST_REQUIREMENTS = [
     'pytest >= 3.7.4',
     'pytest-cov',
+    'webtest >= 1.3.1',
 ]
+FOLDER = dirname(abspath(__file__))
+DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
+    'README.rst', 'CHANGES.rst'])
+
 
 setup(
     name='asset_tracker',
-    version='0.0',
+    version='0.1',
     description='Asset Tracker',
-    long_description=README + '\n\n' + CHANGES,
-    classifiers=[
-        'Programming Language :: Python',
-        'Framework :: Pyramid',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
-    ],
-    author='',
-    author_email='',
+    long_description=DESCRIPTION,
+    classifiers=APP_CLASSIFIERS,
+    author='CrossCompute Inc.',
+    author_email='support@crosscompute.com',
     url='',
     keywords='web pyramid pylons',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    extras_require={
-        'testing': tests_require,
-    },
-    install_requires=requires,
+    extras_require={'testing': TEST_REQUIREMENTS},
+    install_requires=APP_REQUIREMENTS,
     entry_points=ENTRY_POINTS)
