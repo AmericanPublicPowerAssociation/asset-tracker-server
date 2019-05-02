73995c366a711d7617d8d19973a3831b4140e04a
Roy Hyunjin Han <rhh@crosscompute.com>
2019-05-02 19:55:22 -0400

    Define routes

--- asset_tracker/views/assets.py
+++ asset_tracker/views/assets.py
@@ -4,43 +4,38 @@ from pyramid.view import view_config
 @view_config(
     route_name='assets.json',
     renderer='json',
-    request_method='POST',
-)
-def add_assets_json():
+    request_method='GET')
+def see_assets_json(request):
+    pass
+
+
+@view_config(
+    route_name='asset.json',
+    renderer='json',
+    request_method='GET')
+def see_asset_json(request):
     pass
 
 
 @view_config(
     route_name='assets.json',
     renderer='json',
-    request_method='GET',
-)
-def see_assets_json():
+    request_method='POST')
+def add_asset_json(request):
     pass
 
 
 @view_config(
     route_name='asset.json',
     renderer='json',
-    request_method='GET',
-)
-def see_asset_json():
+    request_method='PATCH')
+def change_asset_json(request):
     pass
 
 
 @view_config(
     route_name='asset.json',
     renderer='json',
-    request_method='PATCH',
-)
-def change_asset_json():
-    pass
-
-
-@view_config(
-    route_name='asset.json',
-    renderer='json',
-    request_method='DELETE',
-)
-def drop_asset_json():
+    request_method='DELETE')
+def drop_asset_json(request):
     pass
