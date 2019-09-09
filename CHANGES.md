a4a196bb4f35e91057bf9c48af67444eff9acb86
Roy Hyunjin Han <rhh@crosscompute.com>
2019-09-08 20:09:10 -0400

    Add CHANGES

--- CHANGES.md
+++ CHANGES.md
@@ -1,9 +1,11 @@
+# 0.0.3
+
+- Added /tasks.json
+
 # 0.0.2
 
 - Added /assetsKit.json
-- Removed /assetTypes.json
 
 # 0.0.1
 
 - Added /assets.json
-- Added /assetTypes.json
--- asset_tracker/views/logs.py
+++ asset_tracker/views/logs.py
@@ -1,9 +1,12 @@
 from pyramid.view import view_config
 
+# from ..models import Log
+
 
 @view_config(
     route_name='logs.json',
     renderer='json',
     request_method='GET')
 def see_logs_json(request):
+    # !!! Get logs for utilities to which user has access
     return []
