e68ad6938d38632a2334fc48397383b68fbb8d94
Roy Hyunjin Han <rhh@crosscompute.com>
2019-09-12 02:49:26 -0400

    Add extra attributes

--- asset_tracker/constants.py
+++ asset_tracker/constants.py
@@ -3,8 +3,7 @@ from os.path import dirname, join
 
 
 FOLDER = dirname(__file__)
-ASSET_TYPES = json.load(open(join(
-    FOLDER, 'datasets', 'assetTypes.json')))
+ASSET_TYPES = json.load(open(join(FOLDER, 'datasets', 'assetTypes.json')))
 ASSET_TYPE_BY_ID = {_['id']: _ for _ in ASSET_TYPES}
 RECORD_ID_LENGTH = 8
 RECORD_RETRY_COUNT = 3
