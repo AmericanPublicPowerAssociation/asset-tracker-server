e16fb89e50d0ed883fd9516e02b65caa5d5895db
Roy Hyunjin Han <rhh@crosscompute.com>
2020-08-09 21:40:12 +0000

    Update configuration files

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ APPLICATION_CLASSIFIERS = [
 APPLICATION_REQUIREMENTS = [
     # architecture
     'invisibleroads-posts >= 0.7.8',
-    'invisibleroads-records >= 0.5.6',
+    'invisibleroads-records >= 0.5.7',
     # web
     'pyramid',
     # database
